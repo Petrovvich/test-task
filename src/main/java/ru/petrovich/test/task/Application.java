fc6851c558829c800909d4d74cd52f5d1a6139cb
petrovich <alexromantrue@gmail.com>
2020-02-15 22:45:03 +0700

    init project

---
+++ src/main/java/ru/petrovich/test/task/Application.java
@@ -0,0 +1,18 @@
+package ru.petrovich.test.task;
+
+import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.*;
+import io.swagger.v3.oas.annotations.info.*;
+
+@OpenAPIDefinition(
+    info = @Info(
+            title = "ru-petrovich-test-task",
+            version = "0.0"
+    )
+)
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class);
+    }
+}
